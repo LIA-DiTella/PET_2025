5cfe11577f7ca07258bee085708abe5a687e3694
IgnacioPardo <ignacio.pardo@ort.edu.ar>
2025-06-11 19:25:19 -0300

    Update resnet18_2d.py

--- models/resnet18/resnet18_2d.py
+++ models/resnet18/resnet18_2d.py
@@ -44,7 +44,7 @@ class ResNet18_2D(nn.Module):
 
         # Congelar parámetros si feature_extract=True
         # set_parameter_requires_grad(self.model, feature_extract)
-        set_parameter_requires_grad(self.model)
+        set_parameter_requires_grad(self.model, False)
 
         # Modificar la primera capa convolucional para aceptar imágenes de 1 canal (PET scans)
         # original_weight = self.model.conv1.weight.data
