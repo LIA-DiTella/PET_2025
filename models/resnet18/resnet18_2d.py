a43a8c9a8b75567a1dd38890e69b51d611af12d0
IgnacioPardo <ignacio.pardo@ort.edu.ar>
2025-06-18 18:11:56 -0300

    Update resnet18_2d.py

--- models/resnet18/resnet18_2d.py
+++ models/resnet18/resnet18_2d.py
@@ -61,20 +61,21 @@ class ResNet18_2D(nn.Module):
         #     # nn.Linear(in_features, num_classes),
         # )
         self.model.dropout = nn.Dropout(dropout_rate)
+        # self.model.fc = nn.Sequential(
+        #     # nn.Dropout(dropout_rate),
+        #     nn.Linear(in_features, 1024),
+        #     # nn.LeakyReLU(negative_slope=0.01, inplace=True),
+        #     nn.ReLU(),
+        #     # nn.Dropout(dropout_rate),
+        #     nn.Linear(1024, num_classes),
+        #     nn.Softmax(dim=1)
+        # )
+        
         self.model.fc = nn.Sequential(
             # nn.Dropout(dropout_rate),
-            nn.Linear(in_features, 1024),
-            # nn.LeakyReLU(negative_slope=0.01, inplace=True),
-            nn.ReLU(),
-            # nn.Dropout(dropout_rate),
-            nn.Linear(1024, num_classes),
-            nn.Softmax(dim=1)
+            nn.Linear(in_features, num_classes),
+            nn.Softmax(dim=1)  # Asegurar salida de probabilidades
         )
-        # self.model.fc = nn.Sequential(
-        #     nn.Dropout(dropout_rate),
-        #     nn.Linear(in_features, num_classes),
-        #     nn.Softmax(dim=1)  # Asegurar salida de probabilidades
-        # )
 
     def forward(self, x):
         """Forward pass del modelo.
