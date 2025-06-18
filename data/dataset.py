fix(Dataset): remove ImageNet normalization

data/dataset.py
@@ -425,9 +425,11 @@ class PETDataset(Dataset):
             # print(f"Imagen centrada: forma {img.shape}, etiqueta {label}")
             # img = transforms.functional.normalize(img, mean=np.mean([0.485, 0.456, 0.406]), std=np.mean([0.229, 0.224, 0.225]))  # Normalizar
             img = img.repeat(3, 1, 1)
-            img = transforms.functional.normalize(
-                img, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-            )  # Normalizar
+            
+            # img = transforms.functional.normalize(
+            # img, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+            # )  # Normalizar
+            
             # print(f"Imagen normalizada: forma {img.shape}, etiqueta {label}")
             # img = img[np.newaxis, :, :]  # Añadir dimensión de canal (1, H, W
             # print(f"Imagen con canal añadido: forma {img.shape}, etiqueta {label}")