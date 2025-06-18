9520ec00fa01d59b1aa9a8e111373c2fccccdbbe
IgnacioPardo <ignacio.pardo@ort.edu.ar>
2025-06-18 14:38:26 -0300

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -257,10 +257,11 @@ class PETDataset(Dataset):
 
                 if img_data.ndim == 4:
                     # dynamic_index = 0
-                    dynamic_index = img_data.shape[3] // 2  # Seleccionar el corte medio si es dinámico
-                    img_data = img_data[:, :, :, dynamic_index]
-                    # for j in range(img_data.shape[3]):
-                    #     img_data = img_data[:, :, :, j]
+
+                    # dynamic_index = img_data.shape[3] // 2  # Seleccionar el corte medio si es dinámico
+                    # img_data = img_data[:, :, :, dynamic_index]
+
+                    img_data = np.mean(img_data, axis=3)  # Promediar a lo largo del eje temporal si es dinámico
 
                 # Seleccionar cortes según el método especificado
                 if self.slice_selection == "middle":
@@ -320,7 +321,7 @@ class PETDataset(Dataset):
                         )
 
                         slice_img = ts(slice_img)
-                        
+
                     image[:, :, i] = slice_img
 
                 if not self.is_3d:
@@ -425,11 +426,11 @@ class PETDataset(Dataset):
             # print(f"Imagen centrada: forma {img.shape}, etiqueta {label}")
             # img = transforms.functional.normalize(img, mean=np.mean([0.485, 0.456, 0.406]), std=np.mean([0.229, 0.224, 0.225]))  # Normalizar
             img = img.repeat(3, 1, 1)
-            
+
             # img = transforms.functional.normalize(
             # img, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
             # )  # Normalizar
-            
+
             # print(f"Imagen normalizada: forma {img.shape}, etiqueta {label}")
             # img = img[np.newaxis, :, :]  # Añadir dimensión de canal (1, H, W
             # print(f"Imagen con canal añadido: forma {img.shape}, etiqueta {label}")
