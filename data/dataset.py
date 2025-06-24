e88eee43d2c3ca08d3b79f30749b3929c7ffe5b3
IgnacioPardo <ignacio.pardo@ort.edu.ar>
2025-06-23 22:44:30 -0300

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -490,12 +490,13 @@ class PETDataset(Dataset):
                     )
 
             if self.is_3d:
+                print(img.shape)
                 # Para 3D, convertir a tensor y redimensionar
                 img = transforms.functional.to_tensor(img)
                 # img = transforms.functional.resize(
                 #     img, (self.output_size[0], self.output_size[1], self.num_slices)
                 # )  # ValueError: Size must be an int or a 1 or 2 element tuple/list, not a 3 element tuple/list
-
+                
                 for i in range(img.shape[2]):
                     img[:, :, i] = transforms.functional.resize(
                         img[:, :, i], self.output_size
@@ -511,6 +512,7 @@ class PETDataset(Dataset):
         label = ohe_label
         label = torch.tensor(label, dtype=torch.float32)
 
+        print(img.shape)
         return img, label
 
 
